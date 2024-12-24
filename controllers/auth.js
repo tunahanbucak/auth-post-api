7bda5f0bfd6e72282cb9e2b3b3b28f23914d0438
Tunahan BUCAK <tunahanbucak1@gmail.com>
2024-12-25 01:55:29 +0300

    Added email format verification during registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,14 @@ const jwt = require("jsonwebtoken");
 const register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailRegex.test(email)) {
+      return res
+        .status(400)
+        .json({ message: "Lütfen geçerli bir e-posta adresi girin." });
+    }
+
     const user = await Auth.findOne({ email });
 
     if (user) {
